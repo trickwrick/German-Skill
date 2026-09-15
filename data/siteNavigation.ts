d82c1fb6077a61a4e80ffddee0232c3fa5393b97
Trickwrick <tumhara-trickwrick-github-email@example.com>
2026-09-15 17:42:38 +0530

    Rename Apply Job to Job Portal in footer

--- data/siteNavigation.ts
+++ data/siteNavigation.ts
@@ -36,7 +36,7 @@ const allUsefulLinks = [
   { label: "About", href: "/about/our-company" },
   { label: "Contact Us", href: "/contact" },
   { label: "Blogs", href: "/blogs" },
-  { label: "Apply Job", href: "/about/apply-job" },
+  { label: "Job Portal", href: "/about/apply-job" },
   { label: "FAQs", href: "/about/faqs" },
 ];
 
